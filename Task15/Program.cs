23efc711b371716017faaafb646f67d9f3a10a51
Alexey <alekseynovikov65@gmail.com>
2022-12-24 15:49:14 +0300

    Add Task10,Task13,Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Урок 2.Базовые алгоритмы
+
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+//Console.WriteLine("Урок 2. Задача 15");
+
+Console.WriteLine("Input number: ");
+
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine(A);
+
+if (A <= 5)
+{
+    Console.WriteLine("Это не выходной.");
+}
+else if (A >= 6 && A <= 7)
+{
+    Console.WriteLine("Ура выходной");
+}
+else
+{
+    Console.WriteLine("В недели только 7 дней !!!");
+}
+    
